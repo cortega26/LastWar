b6389bdd4676f5f6396004e3884b2eb9d8fb7893
Carlos Ortega González <carlosortega77@gmail.com>
2025-09-01 21:48:08 -0400

    Delete unused files

--- tooltician-jekyll/tooltician-jekyll-auto/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Protein Farm Calculator",
-        "excerpt":"🏭 Protein Farm Calculator           Farm Level (1-30):                   Production Boost (%):                   Time (hours):              🧮 Calculate   🗑️ Clear             ","categories": [],
-        "tags": [],
-        "url": "/LastWar/calculators/protein-farm/",
-        "teaser": null
-      },]
